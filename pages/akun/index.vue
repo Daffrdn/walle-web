d2bcc65b0e2abc1bec23f6e10b673812221614a7
daffa <daffa.fahrezy@gmail.com>
2022-07-04 13:01:44 +0700

    git init akun-user

---
+++ pages/akun/index.vue
@@ -0,0 +1,29 @@
+<template>
+<div v-if="$store.state.auth.loggedIn">
+  <v-row>
+    <v-col>
+      <AkunComponent/>
+    </v-col>
+  </v-row>
+</div> 
+</template>
+
+<script>
+export default {
+    name: 'AkunPages',
+    layout: "user",
+    middleware: 'auth',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+
+</style>
